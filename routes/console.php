6ed91281ac9257a58c6877d662be6b5eb08e8bcb
MartinT <m@rtin.win>
2025-01-20 20:52:55 +0100

    Run scheduled jobs even in maintenance mode

--- routes/console.php
+++ routes/console.php
@@ -4,5 +4,5 @@ use App\Jobs\CleanStuffJob;
 use App\Jobs\DownloadLatestsGenToolGamesJob;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::job(new DownloadLatestsGenToolGamesJob)->everyTenMinutes();
-Schedule::job(new CleanStuffJob)->dailyAt('05:00');
+Schedule::job(new DownloadLatestsGenToolGamesJob)->evenInMaintenanceMode()->everyTenMinutes();
+Schedule::job(new CleanStuffJob)->evenInMaintenanceMode()->dailyAt('05:00');
